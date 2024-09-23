f5d6153b5253ccab71a88c990231b638eab4a2d1
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-09-23 07:43:15 -0700

    - add links login and signup links to landing page

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="bg-cover bg-no-repeat bg-[url('../app/images/japan-bg.jpg')] min-h-screen flex items-center pl-8">
@@ -8,10 +10,16 @@ export default function Home() {
 
       <section className="ml-14">
         <p>
-          Anything new to share? <b>Login</b>
+          Anything new to share?{" "}
+          <Link href="/login" className="underline text-red-900">
+            Login
+          </Link>
         </p>
         <p>
-          New to Janaru? <b>Sign Up</b>
+          New to Janaru?{" "}
+          <Link href="/signup" className="underline text-red-900">
+            Sign Up
+          </Link>
         </p>
       </section>
     </main>
