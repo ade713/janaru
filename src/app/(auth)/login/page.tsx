52a6fa14fd4419df41badb89bb3771ed542fac04
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-09-22 17:41:04 -0700

    - add headers to page - add link to login on sign up page - add link to sign up on login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,19 @@
 import { AuthForm } from "@/components/auth-form";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-2xl mb-6">Login</h1>
+
       <AuthForm formType="login" />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        No account yet?{" "}
+        <Link href="/signup" className="underline text-green-950">
+          Sign up
+        </Link>
+      </p>
     </main>
   );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,19 @@
 import { AuthForm } from "@/components/auth-form";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-2xl mb-6">Sign Up</h1>
+
       <AuthForm formType="signup" />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account?{" "}
+        <Link href="/login" className="underline text-green-950">
+          Login
+        </Link>
+      </p>
     </main>
   );
 }
