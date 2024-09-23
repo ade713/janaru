6fd78b403d8679357e7558ebde413b9918b0f8ea
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-09-23 07:39:24 -0700

    - add functionality to add entoris

--- src/components/create-entori.tsx
+++ src/components/create-entori.tsx
@@ -1,6 +1,6 @@
 import { EntoriForm } from "./entori-form";
 
-export function CreateEntori() {
+export async function CreateEntori() {
   return (
     <section className="col-span-3 flex flex-col h-full">
       <h2 className="text-xl text-center mb-5">Create Entori</h2>
--- src/components/entori-form.tsx
+++ src/components/entori-form.tsx
@@ -1,16 +1,31 @@
+"use client";
+
+import { SubmitButton } from "@/components/submit-button";
+import { addEntori } from "@/actions/actions";
+
 export function EntoriForm() {
+  const handleAddEntori = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const entoriData = Object.fromEntries(formData.entries());
+
+    e.currentTarget.reset();
+
+    await addEntori(entoriData);
+  };
+
   return (
-    <>
-      <form className="h-1/2 text-center">
-        <label htmlFor="content" />
-        <textarea
-          className="w-full h-full border-2 rounded-md border-green-900/40 bg-yellow-100 mb-3 resize-none"
-          id="content"
-          placeholder="What's up today?"
-          maxLength={500}
-        />
-        <button type="submit">Submit Entori</button>
-      </form>
-    </>
+    <form onSubmit={handleAddEntori} className="h-1/2 text-center">
+      <label htmlFor="content" />
+      <textarea
+        className="w-full h-full border-2 rounded-md border-green-900/40 bg-yellow-100 mb-3 resize-none"
+        id="content"
+        name="content"
+        placeholder="What's up today?"
+        maxLength={500}
+      />
+      <SubmitButton>Submit Entori</SubmitButton>
+    </form>
   );
 }
