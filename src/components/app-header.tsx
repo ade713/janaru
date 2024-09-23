0a388debbc015909e8e19cfc615784eaaa28029a
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-09-23 07:35:49 -0700

    - use components in app header

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,10 +1,13 @@
+import { LogOutButton } from "./log-out-button";
+import UsernameDisplay from "./username-display";
+
 export function AppHeader() {
   return (
     <header className="row-span-1 flex justify-between items-center border-b bg-green-600/80 border-green-950 p-2 text-2xl">
       <h1>Janaru</h1>
-      <nav className="flex gap-4">
-        <h2>User Home</h2>
-        <button>Sign Out</button>
+      <nav className="flex gap-4 items-center">
+        <UsernameDisplay />
+        <LogOutButton />
       </nav>
     </header>
   );
