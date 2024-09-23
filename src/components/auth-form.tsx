37b4a8528cd5e3c13fc7a22a289dca2d1e084244
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-09-22 17:03:16 -0700

    - create auth form

---
+++ src/components/auth-form.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type AuthFormProps = {
+  formType: string;
+};
+
+export function AuthForm({ formType }: AuthFormProps) {
+  const buttonText = formType === "login" ? "Login" : "Sign Up";
+
+  return (
+    <form className="w-72" action="">
+      <div className="flex flex-col mb-3">
+        <label htmlFor="email">Email</label>
+        <input
+          className="border border-green-900 rounded-md p-1 mt-1 bg-yellow-100"
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your email"
+          required
+        />
+      </div>
+      <div className="flex flex-col mb-3">
+        <label htmlFor="password">Password</label>
+        <input
+          className="border border-green-900 rounded-md p-1 mt-1 bg-yellow-100"
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Your password"
+          required
+        />
+      </div>
+      <button type="submit">{buttonText}</button>
+    </form>
+  );
+}
